777f7613a6487bf006bbb99ebd6eb7bc3531ff1f
Guy Bar Yosef <guybaryosef@gmail.com>
2018-10-03 22:34:26 -0400

    final version of pset 2

--- pset1/minicat.c
+++ pset1/minicat.c
@@ -4,24 +4,23 @@
  * By: Guy Bar Yosef 
  *
  * minicat - concatenate and copy files
+ * version: 2.0
  * 
  * To run: 
- *  % gcc -w minicat.c -o minicat
+ *  % gcc minicat.c -o minicat
  *  % ./minicat [-b ###] [-o outfile] infline1 [...infile2...]
  *  % ./minicat [-b ###] [-o outfile] 
  * 
-*  where
-*     - [-b ###] is an optional argument to specify the buffer size in bytes.
-*     - [-o outfile] is an optional argument to specify an output file.
-*     - If no input files are specified, program will read from stdin.
-*     - If the filename ‘-’ is not taken then its usage will direct the program to read from stdin.
- 
+ *  where
+ *     - [-b ###] is an optional argument to specify the buffer size in bytes.
+ *     - [-o outfile] is an optional argument to specify an output file.
+ *     - If no input files are specified, program will read from stdin.
+ *     - If the filename ‘-’ is not taken then its usage will direct the program to read from stdin.
  */
 
 #include <stdio.h>
 #include <string.h> 
 #include <stdlib.h>
-
 #include <errno.h>
 #include <fcntl.h>   
 #include <unistd.h>
@@ -29,29 +28,37 @@
 int main(int argc, char **argv) {
     
     int buf = 4096;           /* default buffer size in bytes */
-    char *buffer = malloc(sizeof(char)*buf);
 
-    char *output = STDOUT_FILENO;    /* output file to write to. Defaults to stdout */
+    char *buffer;
+    if (!(buffer = malloc(sizeof(char)*buf)) ) {
+        fprintf(stderr, "Unable to allocate memory for default buffer size: %s\n", strerror(errno));
+        return -1;
+    }
 
+    int output = STDOUT_FILENO;    /* output file to write to. Defaults to stdout */
     int i = 1; /* iterate through arguments to program */
 
-    if (i < argc && strcmp(argv[i], "-b") == 0) {   /* checking for optional buffer size argument */
+    if (i < argc && !strcmp(argv[i], "-b")) {   /* checking for optional buffer size argument */
         char *tempPt;
         long temp;
         /* check for error in second part of buffer argument */
-        if (++i == argc || (temp = strtol(argv[i], &tempPt, 10)) == 0 || errno == ERANGE)  {
+        if (++i == argc || !(temp = strtol(argv[i], &tempPt, 10)) || errno == ERANGE)  {
             fprintf(stderr, "Invalid argument following optional '-b' flag in minicat program. "
                 "The -b flag is used to specify the buffer size in bytes and requires an integer to follow it.\n");
             return -1;
         }
         else {
             buf = temp;
-            buffer = realloc(buffer, buf);
+            if (!(buffer = realloc(buffer, buf))) {
+                fprintf(stderr, "Unable to allocate the amount of"
+                                "memory requested for buffer: %s\n", strerror(errno) );
+                return -1;
+            }
         }
         ++i;
     }
 
-    if (i < argc && strcmp(argv[i], "-o") == 0) { /* checking for optional output file argument */
+    if (i < argc && !strcmp(argv[i], "-o")) { /* checking for optional output file argument */
 
         if (++i == argc ) {
             fprintf(stderr, "Invalid argument following optional '-o' flag in minicat program. "
@@ -67,52 +74,48 @@ int main(int argc, char **argv) {
  
     do { /* iterate through inputted files */
 
-        char *input;
-        if (i==argc || ( open(argv[i], O_RDONLY) == -1 && strcmp(argv[i], "-") == 0 ) ) /* input will come from stdin */
+        int input;
+        if (i==argc || (open(argv[i], O_RDONLY) == -1 && !strcmp(argv[i], "-"))) /* input will come from stdin */
             input = STDIN_FILENO;
 
         else {  /* reading from file */
             if ( (input = open(argv[i], O_RDONLY)) == -1) { 
-                if (errno = ENOENT)   /* specified file does not exist */
-                    fprintf(stderr, "The specified file name %s is not a valid file name.\n", argv[i]);
-                else 
-                    fprintf(stderr, "Unable to open %s for reading: %s\n", argv[i], strerror(errno));
+                fprintf(stderr, "Unable to open %s for reading: %s\n", argv[i], strerror(errno));
                 return -1;
             }
         }
-        int j = 1;
-        int k;
-        while (j != 0) { /* while we have not reached the end-of-file */
+        int nwt, nrd, tmp; /*num of writing , num of read */
+        while ( nrd = read(input, buffer, buf*sizeof(char)) ) { /* while not at end-of-file */
 
-            j = read(input, buffer, buf*sizeof(char)); /* reading from input */
-            if (j < 0) { 
-                fprintf(stderr, "An error occurred while trying to read from file %s: %s\n", input, strerror(errno));
+            if (nrd < 0) { /* confirm correct read */
+                fprintf(stderr, "An error occurred while trying to"
+                                "read from file descriptor %d: %s\n", input, strerror(errno));
                 return -1;
             }
 
-            k = 0;
-            while (k != j) { /* accounts for partial writes */
+            nwt = 0;
+            while (nwt < nrd) { /* accounts for partial writes */
 
-                k += write(output, buffer + k, j);
-                if ( k <= 0 && j != 0) {    /* error when writing to output file */
-                    fprintf(stderr, "Unable to write buffer to output file %s: %s", output, strerror(errno));
+                if ( (tmp = write(output, buffer + nwt, nrd - nwt)) <= 0 ) {  /* confirm correct write */
+                    fprintf(stderr, "Unable to write buffer to output file %d: %s", output, strerror(errno));
                     return -1;
                 }
+                nwt += tmp;
             }
         }    
         
-        if (close(input) == -1) {
-            fprintf(stderr, "Unable to close output file %s in the shutdown of minicat program: %s\n", output, strerror(errno));
+        if (input != STDERR_FILENO && close(input) == -1) {
+            fprintf(stderr, "Unable to close output file descriptor %d in the"
+                            "shutdown of minicat program: %s\n", output, strerror(errno));
             return -1;
         } 
     } while (++i < argc);
 
     /* if output is a file (not stdout), close it */
-    if (output != STDOUT_FILENO) {
-        if (close(output) == -1) {
-            fprintf(stderr, "Unable to close output file %s in the shutdown of minicat program: %s\n", output, strerror(errno));
-            return -1;
-        } 
+    if (output != STDOUT_FILENO && close(output) == -1) {
+        fprintf(stderr, "Unable to close output file descriptor %d in the" 
+                        "shutdown of minicat program: %s\n", output, strerror(errno));
+        return -1;
     }
 
     return 0;
--- pset2/lister.c
+++ pset2/lister.c
@@ -1,19 +1,25 @@
 /*
  * ECE 357: Computer Operating Systems 
- * Problem Set 2: Problem 2 
+ * Problem Set 2: Problem 3 
  * By: Guy Bar Yosef 
  *
- * lister - Provides a recursive directory listing
+ * lister - Provides a recursive directory listing 
+ *          that copies the format of "find -ls"
+ * version 1
  * 
  * To run: 
- *    % gcc -w lister.c -o lister
+ *    % gcc lister.c -o lister
  *    % ./lister dir
  *    % ./lister  
+ * 
  *  where dir is the starting directory.
  *  If 'dir' is not specified, the program defaults to the current working directory.
+ * 
  */
 
-
+#define MAX_PATH_LEN 256 /* maximum length of path allowed */
+#define MAX_USRN_LEN 32
+#define MAX_GRPN_LEN 32
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -24,23 +30,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <grp.h>
+#include <pwd.h>
 
 
-int recursive_listing(DIR *cur, char *dir_name, FILE* pass, const char *path);
-void parse_mode(int mode, char *output);
-int parse_pass(FILE *pass, char *user, int uid, char *group, int gid);
-void parse_time(long int *mtime, char *output);
+
+int recursive_listing(DIR *cur, char *dir_name, const char *path);
+void parse_mode(const int mode, char *output);
+void parse_pass(char *user, int uid, char *group, const int gid);
+int parse_time(const long int *mtime, char *output);
 
 
 int main(int argc, char ** argv) {
 
-    char start[256];    /* default starting directory */
-    strcpy(start, ".");
+    char start[MAX_PATH_LEN];    
+    strcpy(start, "."); /* default starting directory */
 
     /* confirm correct usage and determine starting directory */
-     if (argc == 2) {
-         strcpy(start, argv[1]);
-    }
+     if (argc == 2)
+         strncpy(start, argv[1], MAX_PATH_LEN);
     else if (argc != 1) {
         fprintf(stderr, "Incorrect usage. Usage: ./lister [starting/dir]\n");
         return -1;
@@ -49,28 +57,17 @@ int main(int argc, char ** argv) {
     /* open specified starting directory */
     DIR *start_dir;
     if ( !(start_dir = opendir(start)) ) { 
-        fprintf(stderr, "Can not open directory %s: %s\n", start, strerror(errno));
-        return -1;
-    }
-
-    /* open passwd file needed to convert user and group numbers to IDs */
-    FILE *pass;
-    if ( !(pass = fopen("/etc/passwd", "r")) ) {
-        fprintf(stderr, "Unable to read from linux password file: %s\n", strerror(errno) );
+        fprintf(stderr, "Cannot open directory %s: %s\n", start, strerror(errno));
         return -1;
     }
 
     /* recursively print all the file info of each file */
-    if (recursive_listing(start_dir, start, pass, start) == -1)
+    if (recursive_listing(start_dir, start, start) == -1)
         return -1;
     
     /* close starting directory and password file */
     if (closedir(start_dir) == -1) {
         fprintf(stderr, "Error closing directory: %s: %s\n", start, strerror(errno) );
-        return -1;
-    }
-    if (fclose(pass) == EOF) {
-        fprintf(stderr, "Error closing password file: %s\n", strerror(errno) );
         return -1;
     }
 
@@ -83,26 +80,35 @@ int main(int argc, char ** argv) {
  * Output for each file is as follows, where (*) occurred and [*] is optional:
  *  (Inode #), (disk usage in 1K units), (inode type and mode string), (link count), (uid name), (gid name),
  *      (file size), mtime, (full path name), [string "->" and contents of the symlink].
+ * NOTE: We will not look at path sizes greater than 256 chars. 
  */
-int recursive_listing(DIR *cur, char *dir_name, FILE* pass, const char *curPath) {
+int recursive_listing(DIR *cur, char *dir_name, const char *curPath) {
     struct dirent *de;
     struct stat st;
 
-    char myPath[256]; /* path of the current file, de */
+    char myPath[MAX_PATH_LEN]; /* path of the current file */
     while (de = readdir(cur)) {
 
         if (strcmp(de->d_name, "..") == 0) /* do not extend scope to parent directories (duh) */
             continue;
 
-        strncpy(myPath, curPath, sizeof(myPath));
+        strcpy(myPath, curPath);
 
-        if (strcmp(de->d_name, ".") != 0) {
+        int pathSizeLeft;
+        if ( strcmp(de->d_name, ".") ) {
+
             myPath[strlen(curPath)] = '/';
             myPath[strlen(curPath)+1] = 0;
-            strncat(myPath, de->d_name, sizeof(myPath) - sizeof(curPath));
+
+            if (( pathSizeLeft = MAX_PATH_LEN - strlen(de->d_name)) < 0) {
+                fprintf(stderr, "Path is too long; I decline to look at paths longer than %d length.\n", MAX_PATH_LEN);
+                return -1;
+            }
+            strncat(myPath, de->d_name, pathSizeLeft + 1);
         }
 
-        if (de->d_type == DT_DIR && strcmp(de->d_name, ".") != 0) {   /* If a directory, recursively call function for it */
+        /* if a directory, enter and list its contents */
+        if (de->d_type == DT_DIR && strcmp(de->d_name, ".")) { 
             
             DIR *new_dir;
             if ( !(new_dir = opendir(myPath)) ) { 
@@ -110,35 +116,40 @@ int recursive_listing(DIR *cur, char *dir_name, FILE* pass, const char *curPath)
                 return -1;
             }
 
-            if (recursive_listing(new_dir, de->d_name, pass, myPath) == -1)
+            if (recursive_listing(new_dir, de->d_name, myPath) == -1)
                 return -1;
 
-            closedir(new_dir);
+            if (closedir(new_dir) == -1) {
+                fprintf(stderr, "Unable to close directory: %s: %s\n", myPath, strerror(errno) );
+                return -1;
+            }
         }
-        else {  /* print out file info to stdout */
+        else {  /* organizing and printing out file info to stdout */
             if (lstat(myPath, &st) == -1) {
-                fprintf(stderr, "Error getting file %s's status: %s\n", myPath, strerror(errno) );
+                fprintf(stderr, "Error getting file %s status: %s\n", myPath, strerror(errno) );
                 return -1;
             }
 
             char mode[11];     /* will hold the parsed file type and premissions */
             parse_mode(st.st_mode, mode);
 
-            char user[255];    /* will hold the username */
-            user[0] = 0;       /* used for error checker in parser */
-            char group[255];   /* will hold the group name */
-            group[0] = 0;      /* used for error checker in parser */
-            if (parse_pass(pass, user, st.st_uid, group, st.st_gid) == -1)
-                return -1;
+            char user[MAX_USRN_LEN];    /* will hold the username */
+            char group[MAX_GRPN_LEN];   /* will hold the group name */
+            parse_pass(user, st.st_uid, group, st.st_gid);
 
             char time_val[13]; /* will hold human-readable mtime value */
-            parse_time(&st.st_mtime, time_val);
+            if (parse_time(&st.st_mtime, time_val) == -1) 
+                return -1;
+
+
 
             if (mode[0] == 'l') { /* if symbolic link, add its contents to printed info */
-                char link[256];
+                char link[MAX_PATH_LEN];
                 strcpy(link, "-> ");
-                char linkName[256];
-                int count = readlink(myPath, linkName, sizeof(linkName));
+                char linkName[MAX_PATH_LEN - 3];
+
+                /* readlink reads a maximum of 'M_P_L - 4', and not '-3', due to strncat behavior */
+                int count = readlink(myPath, linkName, MAX_PATH_LEN - 4); 
                 strncat(link, linkName, count);
 
                 printf("%9ld %6ld %s %3ld %-8s %-8s %8ld %s %s %s\n", st.st_ino, st.st_blocks/2, 
@@ -159,7 +170,7 @@ int recursive_listing(DIR *cur, char *dir_name, FILE* pass, const char *curPath)
 /*
  * Parses the 'stat' function's mode value, converting it into premission notation
  */
-void parse_mode(int mode, char *output) {
+void parse_mode(const int mode, char *output) {
     
     /* find the file type */
     if (S_ISREG(mode))
@@ -195,50 +206,53 @@ void parse_mode(int mode, char *output) {
 
 /*
  * Parse the password file for the relevant user and group names based on their IDs.
- * If the number->name translation is not found, return the number.
+ * If the number->name translation is not found, save into place the number.
+ * Return 0 on completion or -1 on error.
  */
-int parse_pass(FILE *pass, char *user, int uid, char *group, int gid) {
-    fseek(pass, 0, SEEK_SET);
-    char buf[600];
-    char *delim = ":";
-    int user_found = 0;
-    int group_found = 0;
+void parse_pass(char *user, const int uid, char *group, const int gid) {
 
-    while ( fgets(buf, sizeof(buf), pass) != NULL && (!user_found || !group_found) ) {
-        char *name = strtok(buf, delim);     /* current username */
-        strtok(NULL, delim);                 /* hash of password */
-        char *userID = strtok(NULL, delim);  /* user ID */
-        char *groupID = strtok(NULL, delim); /* group ID */
-
-        if (atoi(userID) == uid) {
-            strcpy(user, name);
-            user_found = 1;
-        }
-
-        if (atoi(groupID) == gid) {
-            strcpy(group, name);
-            group_found = 1;
-        }
+    struct passwd *usr;
+    if ( (usr = getpwuid(uid)) != NULL ) {
+        strncpy(user, usr->pw_name, MAX_USRN_LEN-1);
+        user[MAX_USRN_LEN-1] = 0;
     }
-    if (errno) {
-        fprintf(stderr, "Unable to parse password file: %s\n", strerror(errno) );
-        return -1;
+    else
+        snprintf(user, MAX_USRN_LEN-1, "%d", uid);
+
+    struct group *grp;
+    if ( (grp = getgrgid(gid)) != NULL) {
+        strncpy(group, grp->gr_name, MAX_GRPN_LEN-1);
+        user[MAX_GRPN_LEN-1] = 0;
     }
-
-    /* if no number->name translation was found, return the number */
-    if (user[0] == 0)
-        sprintf(user, "%d", uid);
-    if (group[0] == 0)
-        sprintf(group, "%d", gid);
-
-    return 0;
+    else
+        snprintf(group, MAX_GRPN_LEN-1, "%d", gid);
 }
 
 
 /*
- * Parses mtime into human-readable format that matches the 'find' commands formatting
+ * Parses mtime into human-readable format that matches the 'find' command's formatting.
+ * Note that normally the hours and minutes are shown, 
+ * yet if the date is more than 6 months old (or in the future),
+ *  then the year is shown instead of the time.
  */
-void parse_time(long int *mtime, char *output) {
+int parse_time(const long int *mtime, char *output) {
+    
     char *base = ctime(mtime);
-    strncpy(output, base+4, 12);
+    long sixMon = 6*30*24*60*60; /* 6 months in seconds */
+    time_t curTime;
+    if ( (curTime = time(NULL)) == -1 ) {
+        fprintf(stderr, "Could not look up current time: %s\n", strerror(errno) );
+        return -1;
+    }
+
+    if (curTime - sixMon > *mtime || *mtime > curTime) { /* mtime hold date older than 6 months or in future */
+        strncpy(output, base+4, 7);
+        output[7] = 0;
+        strncat(output, base+19, 5);
+    }
+    else {
+        strncpy(output, base+4, 12);
+        output[12] = 0;
+    }
+    return 0;
 }
